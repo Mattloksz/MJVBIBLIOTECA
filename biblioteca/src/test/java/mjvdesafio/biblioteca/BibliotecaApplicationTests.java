6f36efa4d711be437095cf124943bd71bd234356
Mattloksz <matheus.oliveira.pereira05@hotmail.com>
2021-03-29 14:40:33 -0300

    Teste Commit

---
+++ biblioteca/src/test/java/mjvdesafio/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package mjvdesafio.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
